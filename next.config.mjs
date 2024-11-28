f87bf446c720e2f6dde7282fa9e15517f542a943
marlon1985 <marlonfredyp@gmail.com>
2024-11-28 15:55:16 -0500

    correccion

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-    API_URL:"https://chat-clase-g32i951jn-marlons-projects-9cba0676.vercel.app/api/chat/"
-             
+    API_URL:"https://chat-clase-gkyxeb4vf-marlons-projects-9cba0676.vercel.app/api/chat/"
+    
        }
 };
 
